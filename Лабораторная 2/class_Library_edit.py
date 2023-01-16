f6b556d3d2e6b08f46de881f829329808011be35
Daniil-Romanov <115372513+Daniil-Romanov@users.noreply.github.com>
2023-01-16 17:21:06 +0300

    Лабораторная работа 2

---
+++ Лабораторная 2/class_Library_edit.py
@@ -0,0 +1,83 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        """
+        Создание и подготовка к работе объекта "Book"
+
+        :param id_: Идентификатор книги
+        :param name: Название книги
+        :param pages: Количество страниц в книге
+        """
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f'Book(id_={self.id_}, name={self.name!r}, pages={self.pages})'
+
+
+class Library:
+    def __init__(self, books=None):
+        """
+        Создание и подготовка к работе объекта "Library"
+
+        :param books: Список книг
+        """
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+
+    def get_next_book_id(self) -> int:
+        """
+        Метод, возвращающий идентификатор для добавления новой книги в библиотеку.
+        Если книг в библиотеке нет, то вернуть 1.
+        Если книги есть, то вернуть идентификатор последней книги, увеличенный на 1.
+        """
+        if not self.books:
+            return 1
+        else:
+            return self.books[-1].id_ + 1
+
+    def get_index_by_book_id(self, index: int) -> int:
+        """
+        Метод, возвращающий индекс книги в списке, который хранится в атрибуте экземпляра класса.
+        Если книга существует, то вернуть индекс из списка.
+        Если книги нет, то вызвать ошибку ValueError с сообщением:
+        "Книги с запрашиваемым id не существует"
+
+        :param index: Проверяемый id_ книги
+        """
+        for i, book in enumerate(self.books):
+            if index == book.id_:
+                return i
+        raise ValueError("Книги с запрашиваемым id не существует")
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
